6666d3c86f30f718de7a8fcb8420299e7fb12500
Jiannei <longjian.huang@aliyun.com>
2019-10-23 11:41:10 +0800

    feat: contents api

--- src/Api/Repositories/Contents.php
+++ src/Api/Repositories/Contents.php
@@ -15,9 +15,33 @@ use Jiannei\EasyGithub\Api\Api;
 
 class Contents extends Api
 {
+    public function readme($owner, $repo, $params)
+    {
+        return $this->httpClient->get('https://api.github.com/repos/' . rawurlencode($owner) . '/' . rawurlencode($repo) . '/readme',
+            $params);
+    }
+
+    public function show($owner, $repo, $path, $params)
+    {
+        return $this->httpClient->get('https://api.github.com/repos/' . rawurlencode($owner) . '/' . rawurlencode($repo) . '/contents/' . rawurlencode($path),
+            $params);
+    }
+
     public function store($owner, $repo, $path, $params)
     {
         return $this->httpClient->put('https://api.github.com/repos/'.rawurlencode($owner).'/'.rawurlencode($repo).'/contents/'.rawurlencode($path),
+            $params);
+    }
+
+    public function update($owner, $repo, $path, $params)
+    {
+        return $this->httpClient->put('https://api.github.com/repos/' . rawurlencode($owner) . '/' . rawurlencode($repo) . '/contents/' . rawurlencode($path),
+            $params);
+    }
+
+    public function destroy($owner, $repo, $path, $params)
+    {
+        return $this->httpClient->delete('https://api.github.com/repos/' . rawurlencode($owner) . '/' . rawurlencode($repo) . '/contents/' . rawurlencode($path),
             $params);
     }
 }
